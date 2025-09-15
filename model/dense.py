271f30b706b758f45363021b0a16eefe5ab49cf4
EvanMaa <evanmayx7@gmail.com>
2025-09-15 11:15:58 -0400

    outlined dense layer

--- model/dense.py
+++ model/dense.py
@@ -2,3 +2,14 @@
 from model.layer import Layer
 from typing import Tuple
 import cupy as cp
+
+class dense(Layer):
+    def __init__(self, neurons: int, activation_func, input_shape: Tuple[int, int, int, int] = None, first = False):
+        super.__init__(input_shape = input_shape, activation_func = activation_func, first = first)
+        self.neurons = neurons
+
+    def forward(self, A_prev):
+        pass
+
+    def backward(self, gradient):
+        pass
