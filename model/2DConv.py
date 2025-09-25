e9f10831a76e4150e880b12a0ecf6e13a2527a6e
EvanMaa <evanmayx7@gmail.com>
2025-09-25 13:28:09 -0400

    added skeleton of conv layer

--- model/2DConv.py
+++ model/2DConv.py
@@ -2,3 +2,16 @@
 from model.layer import Layer
 from typing import Tuple
 import cupy as cp
+
+class Conv(Layer):
+    def __init__():
+        pass
+    
+    def initialize(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
